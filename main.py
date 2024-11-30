a36b858d5c0c862c725a0d4bc0965afd3e0a8b9a
KafetzisThomas <tomkafetzis06@gmail.com>
2024-11-30 19:56:46 +0200

    Convert frame to grayscale

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import cv2
 # Load video file
 cap = cv2.VideoCapture(sys.argv[1])
 
+
+def detect_lanes(frame):
+    # Convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    return gray
+
+
 while cap.isOpened():
     # Read next frame
     ret, frame = cap.read()
-    # Display the current frame
-    cv2.imshow("frame", frame)
+    gray_frame = detect_lanes(frame)
+    # Display the current frame in Grayscale
+    cv2.imshow("Grayscale", gray_frame)
 
     # Exit loop if key is pressed
     if cv2.waitKey(1) == ord("q"):
