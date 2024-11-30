cc4912cdf748ac563754e83f3dd2fa922ba58a0c
KafetzisThomas <tomkafetzis06@gmail.com>
2024-11-30 22:14:13 +0200

    Apply gaussian blur to reduce noise

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ cap = cv2.VideoCapture(sys.argv[1])
 def detect_lanes(frame):
     # Convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    return gray
+
+    # Apply gaussian blur to reduce noise (from sides)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    return blur
 
 
 while cap.isOpened():
     # Read next frame
     ret, frame = cap.read()
-    gray_frame = detect_lanes(frame)
-    # Display the current frame in Grayscale
-    cv2.imshow("Grayscale", gray_frame)
+    blurred_frame = detect_lanes(frame)
+    # Display the current frame
+    cv2.imshow("tinyslam", blurred_frame)
 
     # Exit loop if key is pressed
     if cv2.waitKey(1) == ord("q"):
