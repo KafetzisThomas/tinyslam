db6d5640acd128895cafeb8f611eab0c6ec7cb7f
KafetzisThomas <tomkafetzis06@gmail.com>
2024-11-30 22:21:46 +0200

    Define & apply ROI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import cv2
+import numpy as np
 
 # Load video file
 cap = cv2.VideoCapture(sys.argv[1])
@@ -15,15 +16,33 @@ def detect_lanes(frame):
     # Perform edge detection
     edges = cv2.Canny(blur, 50, 150)
 
-    return edges
+    # Define ROI mask
+    height, width = edges.shape
+    mask = np.zeros_like(edges)
+    polygon = np.array(
+        [
+            [
+                (int(0.1 * width), height),
+                (int(0.9 * width), height),
+                (int(0.5 * width), int(0.5 * height)),
+            ]
+        ],
+        dtype=np.int32,
+    )
+    cv2.fillPoly(mask, polygon, 255)
+
+    # Apply the mask to the edges
+    masked_edges = cv2.bitwise_and(edges, mask)
+
+    return masked_edges
 
 
 while cap.isOpened():
     # Read next frame
     ret, frame = cap.read()
-    edges_frame = detect_lanes(frame)
+    masked_frame = detect_lanes(frame)
     # Display the current frame
-    cv2.imshow("tinyslam", edges_frame)
+    cv2.imshow("tinyslam", masked_frame)
 
     # Exit loop if key is pressed
     if cv2.waitKey(1) == ord("q"):
